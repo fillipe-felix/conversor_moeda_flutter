b750fffe299d92635eb07b3bf39f907839b735cd
Fillipe Soares <felipesoares_1993@hotmail.com>
2020-06-30 19:46:28 -0300

    Criando o metodo getData() para pegar os dados da requisição e convertelos para JSON e criando a appBar inicial do app

--- lib/main.dart
+++ lib/main.dart
@@ -9,18 +9,44 @@ import 'dart:async';
 //import para converter os dados para json
 import 'dart:convert';
 
-
 const request = "https://api.hgbrasil.com/finance?format=json&key=e4ed03e3";
 
 
 void main() async {
 
-  http.Response response = await http.get(request);
-  print(json.decode(response.body)["results"]["currencies"]["USD"]);
+  //print(await getData());
+  //print(json.decode(response.body)["results"]["currencies"]["USD"]);
 
   runApp(MaterialApp(
-    home: Container(),
+    home: Home(),
   ));
+}
+
+//Função para acessar a Requisição e converter para JSON
+Future<Map> getData() async {
+  http.Response response = await http.get(request);
+  return json.decode(response.body);
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text("\$ Conversor de Moeda \$"),
+        centerTitle: true,
+        backgroundColor: Colors.amber[600],
+      ),
+
+      body: ,
+    );
+  }
 }
 
 
